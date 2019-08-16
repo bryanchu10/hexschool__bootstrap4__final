2721273ce9c3970af7fbb9e512f0497cd9fc3c6e
bryanchu10 <54166428+bryanchu10@users.noreply.github.com>
2019-08-16 15:03:45 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  //quantity
+  $('.quantity').each(function(){
+    var spinner = $(this),
+    input = spinner.find('input[type="number"]'),
+    btnUp = spinner.find('.quantity__btn--up'),
+    btnDown = spinner.find('.quantity__btn--down'),
+    min = input.attr('min'),
+    max = input.attr('max');
+
+    btnUp.click(function(){
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find('input').val(newVal);
+      spinner.find('input').trigger('change');
+    });
+
+    btnDown.click(function(){
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find('input').val(newVal);
+      spinner.find('input').trigger('change');
+    });
+
+  });
+});
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('確定刪除' + recipient + '？')
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
